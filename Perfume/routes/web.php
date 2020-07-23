f9a0094a36e1a21012923b1d95a7c455826274e2
Hernani <50271868+hernanigomes98@users.noreply.github.com>
2020-07-23 02:56:26 -0400

    jasper de MIERDA

--- Perfume/routes/web.php
+++ Perfume/routes/web.php
@@ -12,6 +12,102 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use PHPJasper\PHPJasper;
+
+Route::get('/compilarReporte', function () {
+    $input = base_path() .
+    '/vendor/geekcom/phpjasper/examples/hello_world.jrxml';
+
+    $jasper = new PHPJasper;
+    $jasper->compile($input)->execute();
+
+    return response()->json([
+        'status' => 'ok',
+        'msj' => '¡Reporte compilado!'
+    ]);
+});
+
+Route::get('/reporte', function () {
+    $input = base_path() .
+    '/vendor/geekcom/phpjasper/examples/hello_world.jasper';
+    $output = base_path() .
+    '/vendor/geekcom/phpjasper/examples';
+    $options = [
+        'format' => ['pdf']
+
+    ];
+
+    $jasper = new PHPJasper;
+
+    $jasper->process(
+        $input,
+        $output,
+        $options
+    )->execute();
+
+    $pathToFile = base_path() .
+    '/vendor/geekcom/phpjasper/examples/hello_world.pdf';
+    return response()->file($pathToFile);
+});
+
+Route::get('/listarParametrosReporte', function () {
+    $input = base_path() .
+    '/vendor/geekcom/phpjasper/examples/hello_world_params.jrxml';
+
+    $jasper = new PHPJasper;
+    $output = $jasper->listParameters($input)->execute();
+
+    return response()->json([
+        'status' => 'ok',
+        'parms' => $output
+    ]);
+});
+
+Route::get('/compilarReporteParametros', function () {
+    $input = base_path() .
+    '/vendor/geekcom/phpjasper/examples/hello_world_params.jrxml';
+
+    $jasper = new PHPJasper;
+    $jasper->compile($input)->execute();
+
+    return response()->json([
+        'status' => 'ok',
+        'msj' => '¡Reporte compilado!'
+    ]);
+});
+
+Route::get('/reporteParametros', function () {
+    $input = base_path() .
+    '/vendor/geekcom/phpjasper/examples/hello_world_params.jasper';
+    $output = base_path() .
+    '/vendor/geekcom/phpjasper/examples';
+    $options = [
+        'format' => ['pdf'],
+        'params' => [
+            'myInt' => 7,
+            'myDate' => date('y-m-d'),
+            'myImage' => base_path() .
+            '/vendor/geekcom/phpjasper/examples/jasperreports_logo.png',
+            'myString' => 'Hola Mundo!'
+        ]
+    ];
+
+    $jasper = new PHPJasper;
+
+    $jasper->process(
+        $input,
+        $output,
+        $options
+    )->execute();
+
+    $pathToFile = base_path() .
+    '/vendor/geekcom/phpjasper/examples/hello_world_params.pdf';
+    return response()->file($pathToFile);
+});
+
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
